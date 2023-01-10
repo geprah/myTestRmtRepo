80fda20cfd96c0578306705e4ee70b61297bb310
geprah <geprah@gmail.com>
2023-01-10 10:46:34 +0000

    Created Java Core Project
        pick b32a523 Created Java Core Project
        pick 6c963fe last commits
    	deleted:    .gitignore
    	new file:   DemoApp/.classpath
    	new file:   DemoApp/.gitignore
    	new file:   DemoApp/.project
    	new file:   DemoApp/.settings/org.eclipse.jdt.core.prefs
    	new file:   DemoApp/src/com/vodafone/tecnhology/it_product_delivery/DemoApp.java

---
+++ DemoApp/src/com/vodafone/tecnhology/it_product_delivery/DemoApp.java
@@ -0,0 +1,10 @@
+package com.vodafone.tecnhology.it_product_delivery;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java!");
+	}
+
+}
